440074373d84649db2d4a90ff1299c17030a58ae
Tony <c0224584102@gmail.com>
2015-08-10 17:19:56 +0800

    Remove the addend dependency of 'arithmetic utilization' test case

--- ArithmeticPipelineTest/ArithmeticUtilizationTest/run.sh
+++ ArithmeticPipelineTest/ArithmeticUtilizationTest/run.sh
@@ -1,33 +1,65 @@
 iteration=$1;
 
-./arith_utilization -p 1 -t 0 -k Integer_addition_0_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_addition_5_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_addition_7_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_addition_10_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_addition_12_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_addition_15_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_addition_20_20 -i ${iteration}
+./arith_utilization -p 1 -t 0 -k Integer_addition_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_20_20 -i ${iteration} -g 1024 -l 128
 
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_0_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_5_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_7_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_10_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_12_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_15_20 -i ${iteration}
-./arith_utilization -p 1 -t 0 -k Integer_multiplication_20_20 -i ${iteration}
+./arith_utilization -p 1 -t 0 -k Integer_addition_0_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_5_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_7_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_10_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_12_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_15_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_addition_20_20_dep_free -i ${iteration} -g 1024 -l 128
 
-./arith_utilization -p 1 -t 1 -k Double_addition_0_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_addition_5_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_addition_7_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_addition_10_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_addition_12_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_addition_15_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_addition_20_20 -i ${iteration}
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_20_20 -i ${iteration} -g 1024 -l 128
 
-./arith_utilization -p 1 -t 1 -k Double_multiplication_0_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_multiplication_5_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_multiplication_7_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_multiplication_10_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_multiplication_12_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_multiplication_15_20 -i ${iteration}
-./arith_utilization -p 1 -t 1 -k Double_multiplication_20_20 -i ${iteration}
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_0_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_5_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_7_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_10_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_12_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_15_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 0 -k Integer_multiplication_20_20_dep_free -i ${iteration} -g 1024 -l 128
+
+./arith_utilization -p 1 -t 1 -k Double_addition_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_20_20 -i ${iteration} -g 1024 -l 128
+
+./arith_utilization -p 1 -t 1 -k Double_addition_0_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_5_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_7_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_10_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_12_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_15_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_addition_20_20_dep_free -i ${iteration} -g 1024 -l 128
+
+./arith_utilization -p 1 -t 1 -k Double_multiplication_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_20_20 -i ${iteration} -g 1024 -l 128
+
+./arith_utilization -p 1 -t 1 -k Double_multiplication_0_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_5_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_7_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_10_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_12_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_15_20_dep_free -i ${iteration} -g 1024 -l 128
+./arith_utilization -p 1 -t 1 -k Double_multiplication_20_20_dep_free -i ${iteration} -g 1024 -l 128
--- ArithmeticPipelineTest/ArithmeticUtilizationTest/runPower.sh
+++ ArithmeticPipelineTest/ArithmeticUtilizationTest/runPower.sh
@@ -2,6 +2,8 @@ NVML_DIR=../../NVML
 TEST_NAME=$1
 ITERATION=$2
 TRACE_TIME=$3
+GLOBAL_SIZE=$4
+LOCAL_SIZE=$5
 
 echo "${TEST_NAME}" | grep -q Integer
 if [ $? == 0 ]; then
@@ -12,4 +14,4 @@ fi
 
 ${NVML_DIR}/PowerUsage -t 5 -T ${TRACE_TIME} -o ${NVML_DIR}'/PowerUsageOutput.log' &
 sleep 4s
-./arith_utilization -p 1 -t ${TYPE} -k ${TEST_NAME} -i ${ITERATION} -o ${NVML_DIR}'/KernelExecution.log'
+./arith_utilization -p 1 -t ${TYPE} -k ${TEST_NAME} -i ${ITERATION} -o ${NVML_DIR}'/KernelExecution.log' -g ${GLOBAL_SIZE} -l ${LOCAL_SIZE}
