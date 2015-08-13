beb6ca6bd8087387c99ea2b978fa313844e7b88b
Tony <c0224584102@gmail.com>
2015-08-13 11:40:50 +0800

    Modify shell file to enable the selection of both platform and device

--- ArithmeticPipelineTest/ArithmeticUtilizationTest/MixType/run.sh
+++ ArithmeticPipelineTest/ArithmeticUtilizationTest/MixType/run.sh
@@ -1,12 +1,35 @@
-iteration=$1;
+platform=$1;
+device=$2;
+iteration=$3;
 
-./arith_utilization -p 1 -k Integer_addition_20_Double_addition_10_pattern_1 -i ${iteration} -g 1024 -l 128
-./arith_utilization -p 1 -k Integer_addition_20_Double_addition_10_pattern_2 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_addition_20_20 -i ${iteration} -g 1024 -l 128
 
-./arith_utilization -p 1 -k Integer_addition_40_Double_addition_10_pattern_1 -i ${iteration} -g 1024 -l 128
-./arith_utilization -p 1 -k Integer_addition_40_Double_addition_10_pattern_2 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 0 -k Integer_multiplication_20_20 -i ${iteration} -g 1024 -l 128
 
-./arith_utilization -p 1 -k Integer_addition_40_Double_addition_5_pattern_1 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_addition_20_20 -i ${iteration} -g 1024 -l 128
 
-./arith_utilization -p 1 -k Integer_multiplication_20_Double_multiplication_10_pattern_1 -i ${iteration} -g 1024 -l 128
-./arith_utilization -p 1 -k Integer_multiplication_20_Double_multiplication_10_pattern_2 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_0_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_5_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_7_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_10_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_12_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_15_20 -i ${iteration} -g 1024 -l 128
+./arith_utilization -p ${platform} -d ${device} -t 1 -k Double_multiplication_20_20 -i ${iteration} -g 1024 -l 128
--- ArithmeticPipelineTest/ArithmeticUtilizationTest/MixType/runPower.sh
+++ ArithmeticPipelineTest/ArithmeticUtilizationTest/MixType/runPower.sh
@@ -1,10 +1,19 @@
 NVML_DIR=../../../NVML
 TEST_NAME=$1
-ITERATION=$2
-TRACE_TIME=$3
-GLOBAL_SIZE=$4
-LOCAL_SIZE=$5
+TRACE_TIME=$2
+PLATFORM=$3
+DEVICE=$4
+ITERATION=$5
+GLOBAL_SIZE=$6
+LOCAL_SIZE=$7
+
+echo "${TEST_NAME}" | grep -q Integer
+if [ $?==0 ]; then
+    TYPE=0;
+else
+    TYPE=1;
+fi
 
 ${NVML_DIR}/PowerUsage -t 5 -T ${TRACE_TIME} -o ${NVML_DIR}'/PowerUsageOutput.log' &
 sleep 4s
-./arith_utilization -p 1 -k ${TEST_NAME} -i ${ITERATION} -o ${NVML_DIR}'/KernelExecution.log' -g ${GLOBAL_SIZE} -l ${LOCAL_SIZE}
+./arith_utilization -p ${PLATFORM} -d ${DEVICE} -t ${TYPE} -k ${TEST_NAME} -i ${ITERATION} -o ${NVML_DIR}'/KernelExecution.log' -g ${GLOBAL_SIZE} -l ${LOCAL_SIZE}
