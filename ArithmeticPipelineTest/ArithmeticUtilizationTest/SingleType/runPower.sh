712b3056d439cd4456b28ec6afcb1208fc385d39
Tony <c0224584102@gmail.com>
2015-08-22 19:38:34 +0800

    Modify NVML script in 'Arithmetic utilization' test

--- ArithmeticPipelineTest/ArithmeticUtilizationTest/SingleType/runPower.sh
+++ ArithmeticPipelineTest/ArithmeticUtilizationTest/SingleType/runPower.sh
@@ -8,8 +8,8 @@ TYPE=$6
 GLOBAL_SIZE=$7
 LOCAL_SIZE=$8
 
-${NVML_DIR}/PowerUsage -t 5 -T ${TRACE_TIME} -D ${DEVICE} -o ${NVML_DIR}'/PowerUsageOutput.log' &
-for (( i = 0 ; i < 10 ; i ++ ))
+${NVML_DIR}/PowerUsage -t 10 -T ${TRACE_TIME} -D ${DEVICE} -o ${NVML_DIR}'/PowerUsageOutput.log' &
+for (( i = 0 ; i < 20 ; i ++ ))
 do
     sleep 5s
     ./arith_utilization -p ${PLATFORM} -d ${DEVICE} -t ${TYPE} -k ${TEST_NAME} -i ${ITERATION} -o ${NVML_DIR}'/KernelExecution'${i}'.log' -g ${GLOBAL_SIZE} -l ${LOCAL_SIZE}
