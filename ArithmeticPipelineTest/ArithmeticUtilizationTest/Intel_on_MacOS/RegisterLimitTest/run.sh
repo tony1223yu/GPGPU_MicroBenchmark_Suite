514aac4801bac57fc69caec762098e06c44dda2b
Tony <c0224584102@gmail.com>
2015-09-05 22:12:14 +0800

    Remove old foler

--- ArithmeticPipelineTest/ArithmeticUtilizationTest/Intel_on_MacOS/RegisterLimitTest/run.sh
+++
@@ -1,7 +0,0 @@
-for (( i = 1 ; i <= 20 ; i ++ ))
-do
-    for (( j = 1 ; j <= 20 ; j ++ ))
-    do
-        ./arith_utilization -p 0 -d 0 -k Integer_addition_${i}_no -t 0 -i 100000000 -g 1 -l 1 >> executionTime${i}.log
-    done
-done
